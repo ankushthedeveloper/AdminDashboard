e25b8d6d42024448e09ed88836c87df1cdb89076
ankushthedeveloper <ankushbokra7@gmail.com>
2024-06-03 18:59:08 +0530

    Loading changes

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -1,9 +1,37 @@
 
 
+// const Loader = () => {
+//   return (
+//     <div style={{position:'relative' ,top:'50vh',left:"50vw"}}>Loading..</div>
+//   )
+// }
+
+// export default Loader
+
+
 const Loader = () => {
   return (
-    <div style={{position:'relative' ,top:'50vh',left:"50vw"}}>Loading..</div>
-  )
+    <section className="loader">
+      <div></div>
+    </section>
+  );
+};
+
+export default Loader;
+
+interface SkeletonProps {
+  width?: string;
+  length?: number;
 }
 
-export default Loader
+export const Skeleton = ({ width = "unset", length = 3 }: SkeletonProps) => {
+  const skeletions = Array.from({ length }, (_, idx) => (
+    <div key={idx} className="skeleton-shape"></div>
+  ));
+
+  return (
+    <div className="skeleton-loader" style={{ width }}>
+      {skeletions}
+    </div>
+  );
+};
